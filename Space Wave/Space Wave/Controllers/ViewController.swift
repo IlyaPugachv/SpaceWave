c1c643a4dd014dedea482a924c8bad204a961bdd
Ilya Pugachev <ilyapugachv@gmail.com>
2023-11-10 10:06:38 +0300

    We deleted main and made our own starting view

---
+++ Space Wave/Space Wave/Controllers/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .blue
+    }
+}
+
--- Space Wave/Space Wave/Resources/AppDelegate.swift
+++ Space Wave/Space Wave/Resources/AppDelegate.swift
@@ -1,19 +1,16 @@
-//
-//  AppDelegate.swift
-//  Space Wave
-//
-//  Created by Илья Пугачёв on 9.11.23.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+   
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
