baac87d9231e78d0918a495659ee099399e8494a
Ilya Pugachev <ilyapugachv@gmail.com>
2023-11-15 14:04:33 +0300

    Added application icon + launcher fixes

--- Space Wave/Space Wave/Controllers/Core/LaunchScreenWithAnimationViewController.swift
+++ Space Wave/Space Wave/Controllers/Core/LaunchScreenWithAnimationViewController.swift
@@ -8,8 +8,7 @@ class LaunchScreenWithAnimationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bootScreenDisplay()
-        displayingAppBootScreenAnimation()
+        setBotScreenSettings()
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
             self?.lottieView.stop()
@@ -17,11 +16,18 @@ class LaunchScreenWithAnimationViewController: UIViewController {
         }
     }
     
+    private func setBotScreenSettings() {
+        bootScreenDisplay()
+        displayingAppBootScreenAnimation()
+        view.backgroundColor = .white
+        
+    }
+    
     private func bootScreenDisplay() {
         let label = UILabel()
         label.text = "Space Wave"
-        label.textColor = .white
-        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 35, weight: .bold)
         view.addSubview(label)
         
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -37,7 +43,7 @@ class LaunchScreenWithAnimationViewController: UIViewController {
         lottieView.animationSpeed = 0.7
         lottieView.play()
         
-        lottieView.frame = CGRect(x: view.frame.width/2 - 100, y: 250, width: 150, height: 150)
+        lottieView.frame = CGRect(x: view.frame.width/2 - 100, y: 250, width: 140, height: 140)
         view.addSubview(lottieView)
     }
 }
