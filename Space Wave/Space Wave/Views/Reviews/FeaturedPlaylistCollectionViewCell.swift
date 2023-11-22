4f79283d2a8a09539aac6f0e59cf26822cad1de1
Ilya Pugachev <ilyapugachv@gmail.com>
2023-11-22 16:09:43 +0300

    Version 1.0.2 Added table cells to display the home screen with different playlists of the app. Fixed minor bugs

---
+++ Space Wave/Space Wave/Views/Reviews/FeaturedPlaylistCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeaturedPlaylistCollectionViewCell.swift
+//  Space Wave
+//
+//  Created by Илья Пугачёв on 22.11.23.
+//
+
+import UIKit
+
+class FeaturedPlaylistCollectionViewCell: UICollectionViewCell {
+    static let identifier = "FeaturedPlaylistCollectionViewCell"
+}
---
+++ Space Wave/Space Wave/Views/Reviews/NewReleaseCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NewReleaseCollectionViewCell.swift
+//  Space Wave
+//
+//  Created by Илья Пугачёв on 22.11.23.
+//
+
+import UIKit
+
+class NewReleaseCollectionViewCell: UICollectionViewCell {
+    static let identifier = "NewReleaseCollectionViewCell"
+}
